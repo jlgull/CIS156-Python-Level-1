daacce4b19166f0d2648f0003e14b95dc8efaa2d
Jonathan Heard <jonathan.l.heard@gmail.com>
2022-01-23 08:14:21 -0700

    New directory for CIS156 class.

---
+++ Chapter_2/PA_2-11-1_Numbers_game.py
@@ -0,0 +1,31 @@
+#! / bin / python3
+#
+# Author: Jonathan Heard
+# Work for CIS156, Spring 2022, based on zyBook, CIS156: Python Programming: Level 1
+#
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# 1) Program taken from zyBook, Chapter 2, Additional Practice 2-11-1
+
+num = int(input('Enter 2 digit number:\n'))
+
+result = num * (3 * 7 * 13 * 37)
+
+print(result)
+
+num = int(input('Enter 3 digit number:\n'))
+
+result = num * (7 * 11 * 13)
+
+print(result)
+
+num = int(input('Enter 5 digit number:\n'))
+
+result = num * (11 * 9091)
+
+print(result)
+
