3e1cadbff06df28ae7759f30e0764e26757dd6ff
Jonathan Heard <jonathan.l.heard@gmail.com>
2022-02-09 22:57:32 -0700

    Setting up basic framework for Module 4 assignments.

---
+++ Module_04/payroll.py
@@ -0,0 +1,34 @@
+#! / bin / python3
+#
+# Author: Jonathan Heard
+# Instructor: Tim McMichael
+# Course: CIS156, Section 36323
+# Work for CIS156, based on zyBook, CIS156: Python Programming: Level 1
+#
+# Program name: payroll.py
+#
+"""
+
+Ch 4 Programming Assignment, Part B
+
+Renee's Rad Restaurant pays employees differently depending on their job title and experience.
+    In a program called payroll.py, use separate prompts to collect the following input:
+
+    Job title ("W" for wait staff, "H" for host, "K" for kitchen staff)
+    Number of hours worked
+    Number of years employed at the restaurant
+
+Then, output their gross pay amount (i.e., the amount they earn before taxes and other deductions)
+    based on the following:
+
+    Wait staff earns $5/hour until they've worked 1 year, then earn $6/hour
+    Hosts earn $12/hour for the first 2 years, then go up to $13.50/hour
+    Kitchen staff make $15/hour for the first year, then go to $18/hour
+
+Note: At this stage, you do not need to worry about "rounding off" the numbers to look like currency.
+    It's okay if they have an incorrect number of decimal places.
+
+"""
+
+
+# End of program
---
+++ Module_04/phone_service.py
@@ -0,0 +1,36 @@
+#! / bin / python3
+#
+# Author: Jonathan Heard
+# Instructor: Tim McMichael
+# Course: CIS156, Section 36323
+# Work for CIS156, based on zyBook, CIS156: Python Programming: Level 1
+#
+# Program name: phone_service.py
+#
+"""
+
+Ch 4 Programming Assignment, Part A
+
+EMobile Phones is a new provider of cellular phone service for students at EMCC. Write a program called
+    phone_service.py to help customers choose a service plan.
+    Prompt the user to input the following information:
+
+    Maximum number of "talk" minutes
+    Maximum number of SMS/text messges
+    Maximum amount of data, in gigabytes.
+
+Based on the input, use branching to recommend appropriate service plan options:
+
+    A customer who needs fewer than 500 minutes of talk and no text or data should accept
+        Plan A at $49 per month.
+    A customer who needs fewer than 500 minutes of talk and any text messages--but no data--should accept
+        Plan B at $55 per month.
+    A customer who needs 500 or more minutes of talk and no data should accept either
+        Plan C for up to 100 text messages at $61 per month or
+        Plan D for 100 text messages or more at $70 per month.
+    A customer who needs any data should accept
+        Plan E for up to 3 gigabytes at $79 or Plan F for 3 gigabytes or more at $87.
+"""
+
+
+# End of program
