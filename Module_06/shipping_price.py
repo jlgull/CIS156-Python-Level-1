7586bab1372555030cc8c1085bc32d70ae9f9ade
Jonathan Heard <jonathan.l.heard@gmail.com>
2022-02-26 12:55:51 -0700

    Changes to the Chapter 6, Part A program, total_cash.py, to allow for using the program again. Finished the Chapter 6, Part B program, shipping_price.py. Included the ability to run the program again.

--- Module_06/shipping_price.py
+++ Module_06/shipping_price.py
@@ -29,19 +29,119 @@ Display this returned value for the user.
 Variable List
 
 String variables:
-play_again  - will collect the response for running the program again
+    do_again  -   will collect the response for running the program again
 
 Floating point numbers:
+In main program (shipping_price.py):
+    total_weight    -   weight of items being shipped.
+    total_ship_cost -   shipping cost calculated in function 
+
+In function (calc_shipping_cost):
+    weight      -   total_weight passed to the function
+    cost        -   cost determined in function and returned
 
 Integers numbers: 
-start_num   - number entered to start summing process
-temp_start  - holds start, to be used if start is greater than end
-end_num     - number entered to end summing process
-temp_end    - holds end, to be used if start is greater than end
-sum_total   - the total of the summing process
+In main program (shipping_price.py):
+    items_count     -   the number of items to be shipped
+
+In function (calc_shipping_cost):
+    count       -   items_count, passed to the function 
+
+
+"""
+
+"""
+# Define the function to calculate shipping costs,
+#   based on the number of items being shipped
+#   and the total weight of all the items being shipped.
+
+Then pass that data to a user-defined function that accepts both values as parameters and
+    returns (not outputs) a shipping price based on the following guidelines:
+
+    Less than 3 items and less than one pound: $5.00 shipping charge.
+    Less than 3 items and one pound or over: $7.50 shipping charge.
+    3 to 5 items and less than two pounds: $8.50 shipping charge.
+    Anything else: $10.00 shipping charge.
 
 """
 
 
+def calc_cost(count, weight):
+    # If the count is less than 3.
+    if count < 3:
+        cost = 7.5 if weight >= 1 else 5.0
+
+    # If the count is 3 to 5 items and less than two pounds.
+    elif (3 <= count <= 5) and weight < 2.0:
+        cost = 8.5
+
+    # If anything else.
+    else:
+        cost = 10
+
+    # Return the shipping cost.
+    return cost
+
+
+# End of Function definition
+
+# Set the while control value to "Y".
+do_again = "Y"
+
+# Use while, regarding the desire to re-run the program.
+while do_again != "N":
+
+    # Main program
+    # Use a prompt to get (from the user) the number of items
+    # and the total weight of the package (in pounds).
+    # Using while and try to ensure that the correct data type is entered
+
+    # Collect the number of items be shipped, it must be an integer.
+    while True:
+        try:
+            items_count = int(input("\nPlease enter the number of items being shipped. "))
+        except:
+            print("Your entry was not an integer, please enter a number.")
+        else:
+            break
+
+    # Collect the weight of items be shipped, it must be a floating point number.
+    while True:
+        try:
+            total_weight = float(input("Enter the total weight in pounds, as a decimal value. "))
+        except:
+            print("Your entry was not a floating point number, try again.")
+        else:
+            break
+
+    # Call the function, to calculate the total shipping cost.
+    total_ship_cost = calc_cost(items_count, total_weight)
+
+    # Display this returned value for the user.
+    # Print statement to print the returned value.
+    # Print a blank line between the data entered and the results.
+    print("\n")
+
+    # If there is only one item, use the singular form of item.
+    if items_count == 1:
+        print(f"To ship your {items_count} item,", end="")
+    else:
+        print(f"To ship your {items_count} items", end="")
+
+    # Print the remainder of the message.
+    print(f" which weights {total_weight:.2f} pounds.")
+    print(f"Your shipping charge is ${total_ship_cost:.2f}.")
+
+    # The following code as copied from the sum_loop program.
+    # Ask if the user would like to repeat the program.
+    # Also, validate for the correct response.
+    while True:
+        print("\nWould you like to calculate your shipping charges again? Enter (Y) for yes or (N) for no.", end=" ")
+        do_again = input()
+        do_again = do_again.upper()
+        if do_again == "N" or do_again == "Y":
+            break
+        else:
+            print("The only valid entries are either a Y or an N.")
 
 # End of program
--- Module_06/total_cash.py
+++ Module_06/total_cash.py
@@ -27,38 +27,41 @@ For the variables related to the bill denomination, I decided to use Roman Numer
     denominations. I for $1s, II for $2s, V for $5s, X for $10s, XX for $20, L for $50 and C for $100.
     This just allowed for simpler variable identification. The a "c" was added for use within the function.  
 
-
-
 Variable List
 
 Since there are no arrays in Python, so I used a list. Here is information about the (array) List.
 List info:
 
-bill_array  - is defined to hold several items related of the denominations in the wallet.
-                There are 3 fields for each denomination.
-                The 1st field is the generic name for the denomination - 'Ones'
-                The 2nd field is an integer, representing the value of the denomination     - 1
-                The 3rd field is an integer, representing the count of the denomination     -  'I_count'
+    bill_array  -   is defined to hold several items related of the denominations in the wallet.
+                    This is also being used as a global variable, both inside the function and the
+                        main program.
+                    There are 3 fields for each denomination.
+                        The 1st field is the generic name for the denomination  - 'Ones'
+                        The 2nd field is an integer, representing the value of the denomination  - 1
+                        The 3rd field is an integer, representing the count of the denomination  -  'I_count'
 
 String variables:
-input_string    - This is the raw, string value collected by input.
+In main program (total_cash.py):
+    do_again      -   will collect the response for running the program again
+    input_string    - This is the raw, string value collected by input.
                     This value is tested to ensure it is a number and then converted
                     to an integer, if a number. If not a number a message is displayed
                     and a new request is made.
+
+In function (count_bills):
                   
 Floating point numbers:
+In main program (total_cash.py):
+  
+In function (count_bills):
 
 Integers numbers: 
-    I_c     - is the count of $1s being passed to the function.
-    II_c    - is the count of $2s being passed to the function.
-    V_c     - is the count of $5s being passed to the function.
-    X_c     - is the count of $10s being passed to the function.
-    XX_c    - is the count of $20s being passed to the function.
-    L_c     - is the count of $50s being passed to the function.
-    C_c     - is the count of $100s being passed to the function.
-total_cash  - is the total of the cash in the wallet, calculated in the function and returned.
-    i & t   - are just index variables used in for loops.
-    
+In main program (total_cash.py):
+    total_cash  -   The sum of the bills, based on data returned from the function.
+      
+In function (count_bills):
+    cash_sum    -   The sum of the bills, calculated in the function and returned.  
+   
 """
 
 # Define the bill_array, which will be used throughout the program.
@@ -75,73 +78,94 @@ bill_array = ['Ones', 1, 'I_count',
 """
 It should then call a function that accepts each of those values as arguments/parameters and
     returns (not prints!) the total amount of the cash.
+
 Here is the function to turn a count of the number of bills in your wallet or hand
    into a total cash amount. Since I only wanted to define the bill_array once, it was included
    as a global variable in the function.
 
- Determined I could use a global value, instead of passing the 7 different
-   values. 
+ Determined I could use a global value, instead of passing the 7 different values. 
  # def count_bills(I_c=0, II_c=0, V_c=0, X_c=0, XX_c=0, L_c=0, C_c=0):
  """
 
 
 def count_bills():
+    # Use a global variable, so that it isn't necessary to pass
+    #   data during the call of the program.
     global bill_array
 
     # Clear or set the total_cash variable.
     cash_sum = 0
 
-    # Use a for loop to total the cash, based on values supplied by the call.
+    # Use a for loop to total the cash based on data entered in the calling program.
     for t in range(0, len(bill_array), 3):
         cash_sum = cash_sum + (bill_array[t + 1] * bill_array[t + 2])
     return cash_sum
 
 
-# Main body of the program.
-# It should ask the user to input the quantity of each denomination of bills
-#     (ones, fives, etc--up to hundreds).
+# Set the while control value to "Y".
+do_again = "Y"
 
-# Use a for loop to collect how many of each bill denomination is to be summed.
-for i in range(0, len(bill_array), 3):
+# Use while, regarding the desire to re-run the program.
+while do_again != "N":
+
+    # Main body of the program.
+    # It should ask the user to input the quantity of each denomination of bills
+    #     (ones, fives, etc--up to hundreds).
+    # Print a blank line between the data entered and the results.
+    print("\n")
+
+    # Use a for loop to collect how many of each bill denomination is to be summed.
+    for i in range(0, len(bill_array), 3):
+        while True:
+            print(f"Please enter the number of ${bill_array[i + 1]} bills in your wallet: ", end='')
+            input_string = input()
+
+            # If the user enters nothing, the input value will be set to 0.
+            if input_string == "":
+                input_string = "0"
+
+            # Test to ensure that a number between 0 and 9 was entered. The input was a string value; but
+            #   only an integer is needed. This also prevents a none number entry from crashing the program.
+            if '0' <= input_string <= '99':
+                bill_array[i + 2] = int(input_string)
+                break
+            print("The entry must be a number between 0 and 99.\n")
+
+    # Call the function, with the data that was entered.
+    """
+    This is how I first tried to pass the data to the function. Found I could use a global value
+      to do the same thing. 
+    total_cash = count_bills(I_c=bill_array[2], II_c=bill_array[5],
+                             V_c=bill_array[8], X_c=bill_array[11],
+                             XX_c=bill_array[14], L_c=bill_array[17],
+                             C_c=bill_array[20])
+    """
+
+    total_cash = count_bills()
+
+    # Print out the data supplied and the cash total, as well.
+    # Don't print denomination when the user reported that they didn't have any of that denomination.
+    print("\nIn your wallet you reported:")
+    for i in range(0, len(bill_array), 3):
+        if bill_array[i + 2] == 0:
+            continue
+        elif bill_array[i + 2] == 1:
+            print(f"    {bill_array[i + 2]} - ${bill_array[i + 1]} bill")
+        else:
+            print(f"    {bill_array[i+2]} - ${bill_array[i + 1]} bills")
+
+    print(f"\nThis totals to ${total_cash} cash in your wallet.")
+
+    # The following code as copied from the sum_loop program.
+    # Ask if the user would like to repeat the program.
+    # Also, validate for the correct response.
     while True:
-        print(f"Please enter the number of ${bill_array[i + 1]} bills in your wallet: ", end='')
-        input_string = input()
-
-        # If the user enters nothing, the input value will be set to 0.
-        if input_string == "":
-            input_string = "0"
-
-        # Test to ensure that a number between 0 and 9 was entered. The input was a string value; but
-        #   only an integer is needed. This also prevents a none number entry from crashing the program.
-        if '0' <= input_string <= '9':
-            bill_array[i + 2] = int(input_string)
+        print("\nWould you like to calculate the sum of your bills again? Enter (Y) for yes or (N) for no.", end=" ")
+        do_again = input()
+        do_again = do_again.upper()
+        if do_again == "N" or do_again == "Y":
             break
-        print("The entry must be a number between 0 and 9.\n")
-
-# Call the function, with the data that was entered.
-"""
-# This is how I first tried to pass the data to the function. Found I could use a global value
-#   to do the same thing. 
-total_cash = count_bills(I_c=bill_array[2], II_c=bill_array[5],
-                         V_c=bill_array[8], X_c=bill_array[11],
-                         XX_c=bill_array[14], L_c=bill_array[17],
-                         C_c=bill_array[20])
-"""
-
-total_cash = count_bills()
-
-# Print out the data supp
-# lied and the cash total, as well. Don't print denomination when the
-#   user reported that they didn't have any of that denomination.
-print("\nIn your wallet you reported:")
-for i in range(0, len(bill_array), 3):
-    if bill_array[i + 2] == 0:
-        continue
-    elif bill_array[i + 2] == 1:
-        print(f"    {bill_array[i + 2]} - ${bill_array[i + 1]} bill")
-    else:
-        print(f"    {bill_array[i+2]} - ${bill_array[i + 1]} bills")
-
-print(f"\nThis totals to ${total_cash} cash in your wallet.")
+        else:
+            print("The only valid entries are either a Y or an N.")
 
 # End of program
