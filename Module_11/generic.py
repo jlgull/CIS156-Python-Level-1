4c901aebedd0b2dd259c63817ac9f527bc0c0422
Jonathan Heard <jonathan.l.heard@gmail.com>
2022-03-03 20:32:01 -0700

    Created directories and generic.py for all the remaining chapters.

---
+++ Module_11/generic.py
@@ -0,0 +1,73 @@
+#! / bin / python3
+#
+# Author: Jonathan Heard
+# Instructor: Tim McMichael
+# Course: CIS156, Section 36323
+# Work for CIS156, based on zyBook, CIS156: Python Programming: Level 1
+#
+# Program name: generic.py
+#
+"""
+Generic shell for the CIS156 programs
+
+Ch XX Programming Assignment, Part ??
+
+Copy instructions from Canvas, change the program name as required.
+
+
+"""
+
+"""
+
+Variable List
+
+Since there are no arrays in Python, so I used a list. Here is information about the (array) List.
+List info:
+
+
+String variables:
+In main program (total_cash.py):
+
+In function (count_bills):
+                  
+Floating point numbers:
+In main program (total_cash.py):
+  
+In function (count_bills):
+
+Integers numbers: 
+In main program (total_cash.py):
+      
+In function (count_bills):
+   
+"""
+
+# Function definition section of the program
+
+"""
+
+ """
+
+
+# Set the while control value to "Y".
+do_again = "Y"
+
+# Use while, regarding the desire to re-run the program.
+while do_again != "N":
+
+    # Main body of the program.
+
+
+    # The following code as copied from the sum_loop program.
+    # Ask if the user would like to repeat the program.
+    # Also, validate for the correct response.
+    while True:
+        print("\nWould you like to calculate the sum of your bills again? Enter (Y) for yes or (N) for no.", end=" ")
+        do_again = input()
+        do_again = do_again.upper()
+        if do_again == "N" or do_again == "Y":
+            break
+        else:
+            print("The only valid entries are either a Y or an N.")
+
+# End of program
