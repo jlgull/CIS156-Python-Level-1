f2125a7534fc2cb77e9331d8b001d5c8ad5e169b
Jonathan Heard <jonathan.l.heard@gmail.com>
2022-03-30 15:11:06 -0700

    Chapter 10, Part A, paint_cost.py is complete, all testing and changes incorporated.

--- Module_10/Play/play.py
+++ Module_10/Play/play.py
@@ -13,29 +13,8 @@
 # Define functions
 #
 
-user_input = ''
-while user_input != 'q':
-    try:
-        weight = int(input('Enter weight (in pounds): '))
-        if weight < 0:
-            raise ValueError('Invalid weight.')
-
-        height = int(input('Enter height (in inches): '))
-        if height <= 0:
-            raise ValueError('Invalid height.')
-
-        bmi = (float(weight) * 703) / (float(height * height))
-        print('BMI:', bmi)
-        print('(CDC: 18.6-24.9 normal)\n')
-        # Source www.cdc.gov
-
-    except ValueError as excpt:
-        print(excpt)
-        print('Could not calculate health info.\n')
-
-    user_input = input("Enter any key ('q' to quit): ")
-
-
+x = float(input("Enter x: "))
+print(x)
 
 
 # End of program
--- Module_10/paint_cost.py
+++ Module_10/paint_cost.py
@@ -7,6 +7,14 @@
 #
 # Program name: paint_cost.py
 #
+
+# Import all required options
+#
+# Import system and name from os
+from os import system, name
+
+# End of import section
+
 """
 Ch 10 Programming Assignment, Part A
 
@@ -29,13 +37,16 @@ List info:
 
 String variables:
     In main program:
+        do_again    -   Used in the control of looping the program again.
 
     In function:
         name    -   In the clear function, finds the type of OS being used.
         system  -   In the clear function, holds the appropriate OS clear command.  
-        label1          -   Used in both dimensional functions, to define data being gathered.
-        entered_feet    -   Used in the feet dimensional function, to hold the value in feet.
-        entered_inches  -   Used in the inches dimensional function, to hold the value in inches.
+        
+        label1          -   Used in dimensional function, to define data being gathered.
+        units           -   Used in dimensional function, to define data being gathered.
+        entered_data    -   Used in dimensional function, to hold the value entered.
+        lower_limit     -   Used in dimensional function for comparison, while checking the data entry.
                   
 Floating point numbers:
     In main program:
@@ -52,15 +63,6 @@ Integers numbers:
    
 """
 
-#
-# Import all required options
-#
-# Import system and name from os
-from os import system, name
-
-# End of import section
-
-
 # Function definition section of the program.
 
 
@@ -70,46 +72,43 @@ def clear():
     # which is found under the "Edit run configuration" tab.
 
     # for windows
-    if name == 'nt':
-        _ = system('cls')
+    if name == "nt":
+        _ = system("cls")
 
-    # for mac and linux(here, os.name is 'posix')
+    # for mac and linux(here, os.name is "posix")
     else:
-        _ = system('clear')
+        _ = system("clear")
 
 
-def get_feet(label1):
-    # Function to request a wall dimension in feet and
-    # return the value.
-    # label1    - which dimension   (width or height) of wall.
-    entered_feet = input(f"\nEnter {label1} of the wall in feet: ")
-
-    # Error message if the entry is a string or text.
-    if entered_feet.isalpha():
-        raise ValueError('Only numbers are allowed.')
-    entered_feet = float(entered_feet)
-
-    # Error message if the entry is less than 0.
-    if entered_feet < 0:
-        raise ValueError('Entry for feet was less than 0.')
-    return entered_feet
-
-
-def get_partial_feet(label1):
-    # Function to request the additional wall dimension in inches and
-    # return it in partial feet.
+def get_data(label1, units):
+    # Function to request a wall dimension in the units requested,
+    # return the value and test for validity.
     # label1    - which dimension (width or height) of wall.
-    entered_inches = input(f"Enter{label1} of the wall in inches: ")
+    # units     - the units being requested, feet or inches.
+    while True:
 
-    # Error message if the entry is a string or text.
-    if entered_inches.isalpha():
-        raise ValueError('Only numbers are allowed.')
-    entered_inches = float(entered_inches)
+        # Setup to request data and convert it to a floating point number,
+        #   also prepare for invalid data type.
+        try:
+            entered_data = float(input(f"Enter {label1} of the wall in {units}: "))
+        except ValueError:
+            print("Only numbers are allowed.")
+            continue
 
-    # Error message if the entry is less than 0.
-    if entered_inches < 0:
-        raise ValueError('Entry for inches was less than 0.')
-    return entered_inches / 12
+        # Setup to validate that the number is in a valid range.
+        try:
+            # Use a ternary to set the lower_limit variable.
+            lower_limit = 0 if units == "inches" else 1
+
+            # Test that the data is above the lower_limit set above.
+            if entered_data < lower_limit:
+                raise ValueError(f"Entry for {units} was {entered_data}, which is less than {lower_limit}.")
+        except ValueError as error_info:
+            print(error_info)
+            continue
+        break
+    # Call a function to test entered data.
+    return entered_data
 
 
 # End of Function definition section.
@@ -125,7 +124,7 @@ while do_again != "N":
     # Create a program called paint_cost.py that prompts the user to input the height of a wall (in feet),
     #   the width of the wall (in feet), and the cost of the paint used to cover it.
     # Prior to starting the program, clear the screen.
-    # If the program is rerun, then clear the screen as well.
+    #   If the program is rerun, this will clear the screen as well.
     clear()
 
     # Print statement to describe the program and expected input data.
@@ -134,35 +133,40 @@ while do_again != "N":
           "  The output of the program will be the wall dimensions, the wall area, the paint cost\n"
           "    and the cost per square foot for the completed job.")
 
-    try:
+    # Print a blank line, prior to gathering data.
+    print()
 
-        # Call the 2 functions (get_feet) and (get_partial_feet) to gather
-        # the rooms dimensions.
-        wall_width = get_feet("width") + get_partial_feet("width")
-        wall_height = get_feet("height") + get_partial_feet("height")
+    # Call the function (get_data) to gather the rooms dimensions.
+    wall_width = get_data("width", "feet") + get_data("width", "inches") / 12
+    wall_height = get_data("height", "feet") + get_data("height", "inches") / 12
 
+    # Calculate the wall area.
+    wall_area = wall_width * wall_height
 
-        # Calculate the wall area
-        wall_area = wall_width * wall_height
+    # Get the paint cost for the wall that was painted.
+    # Test for valid entry as well.
+    while True:
+        try:
+            paint_cost = float(input("\nEnter the cost of the paint used. For a cost of $3.25, enter 3.25: "))
+        except ValueError:
+            print("Only numbers are allowed.")
+            continue
+        try:
 
-        # Get the paint cost for the wall that was painted.
-        paint_cost = float(input("\nEnter the cost of the paint used. For a cost of $3.25, enter 3.25: "))
+            # Test that the data is above the lower_limit set above.
+            if paint_cost < 0:
+                raise ValueError(f"Entry for paint was less than $0.00.")
+        except ValueError as error_feedback:
+            print(error_feedback)
+            continue
+        break
 
-        # Output the area of the wall (height x width) and the cost per square foot (cost / area).
-        # Here is the printed output of program.
-        print(f"\nThe wall is {wall_width:.2f} feet wide and {wall_height:.2f} feet tall.")
-        print(f"The total wall area is {wall_area:.2f} Square feet.")
-        print(f"Based on the paint cost of ${paint_cost:.2f}, the cost per Square Foot is $", end="")
-        print(f"{paint_cost / wall_area:.2f}.")
-
-
-    except ValueError as excpt:
-        print(excpt)
-        print('Could not calculate painting costs from the information supplied.\n')
-
-    except ValueError:
-        print("Only numbers are allowed, no text entries.")
-
+    # Output the area of the wall (height x width) and the cost per square foot (cost / area).
+    # Here is the printed output of program.
+    print(f"\nThe wall is {wall_width:.2f} feet wide and {wall_height:.2f} feet tall.")
+    print(f"The total wall area is {wall_area:.2f} Square feet.")
+    print(f"Based on the paint cost of ${paint_cost:.2f}, the cost per Square Foot is $", end="")
+    print(f"{paint_cost / wall_area:.2f}.")
 
     # Ask if the user would like to repeat the program.
     # Also, validate for the correct response.
